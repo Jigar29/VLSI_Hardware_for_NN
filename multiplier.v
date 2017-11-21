6731b96f9daf6282b9d4cb4c67093444d829c99e
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-11-21 07:22:58 -0800

    Module name chneged for multiplier nd adder, no major fix

--- multiplier.v
+++ multiplier.v
@@ -1,4 +1,4 @@
-module mult(output[15:0] result, input clk, input wire[7:0] multiplicand, input wire[7:0] multiplier);
+module mult16(output[15:0] result, input clk, input wire[7:0] multiplicand, input wire[7:0] multiplier);
 		
 	parameter start = 4'b0000, s0 = 4'b0001, s1 = 4'b0010, s2 = 4'b0011, s3 =4'b0100, s4 = 4'b0101, s5 = 4'b0110, s6 = 4'b0111, s7 = 4'b1000, s8 = 4'b1001, s9 = 4'b1010, s10 = 4'b1011, s11= 4'b1100, s12 = 4'b1101, s14 = 4'b1110, stop = 4'b1111;
 	parameter BIT_WIDTH = 8; 
