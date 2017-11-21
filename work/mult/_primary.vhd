07538a42f2e21d06e6488fc079a1adb36836ed9a
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-11-20 21:10:51 -0800

    16 bit multiplier addedd

--- work/mult/_primary.vhd
+++ work/mult/_primary.vhd
@@ -18,13 +18,13 @@ entity mult is
         s12             : vl_logic_vector(0 to 3) := (Hi1, Hi1, Hi0, Hi1);
         s14             : vl_logic_vector(0 to 3) := (Hi1, Hi1, Hi1, Hi0);
         stop            : vl_logic_vector(0 to 3) := (Hi1, Hi1, Hi1, Hi1);
-        BIT_WIDTH       : integer := 4
+        BIT_WIDTH       : integer := 8
     );
     port(
-        result          : out    vl_logic_vector(7 downto 0);
+        result          : out    vl_logic_vector(16 downto 0);
         clk             : in     vl_logic;
-        multiplicand    : in     vl_logic_vector(3 downto 0);
-        multiplier      : in     vl_logic_vector(3 downto 0)
+        multiplicand    : in     vl_logic_vector(7 downto 0);
+        multiplier      : in     vl_logic_vector(7 downto 0)
     );
     attribute mti_svvh_generic_type : integer;
     attribute mti_svvh_generic_type of start : constant is 1;
