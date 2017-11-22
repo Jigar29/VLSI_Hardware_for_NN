3b97b515bdc31e081cdc3b81eeb180166cb57f68
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-11-22 11:58:44 -0800

    Adder included with reset and the done flags

--- adder.v
+++ adder.v
@@ -1,4 +1,4 @@
-module adder16(output[15:0] result, input clk, input[15:0] x, input[15:0] y); 
+module adder16(output[15:0] result, output done, input clk, input reset, input[15:0] x, input[15:0] y); 
 	parameter start = 4'b0000, s0 = 4'b0001, s1 = 4'b0010, s2 = 4'b0011, s3 =4'b0100, s4 = 4'b0101, s5 = 4'b0110, s6 = 4'b0111, s7 = 4'b1000, s8 = 4'b1001, s9 = 4'b1010, s10 = 4'b1011, s11= 4'b1100, s12 = 4'b1101, s14 = 4'b1110, stop = 4'b1111;
 	parameter BIT_WIDTH = 16; 
 	
@@ -10,18 +10,24 @@ module adder16(output[15:0] result, input clk, input[15:0] x, input[15:0] y);
 	reg overflow, cout;
 	
 	assign result = final_sum; 
+	assign done = (pre_st == stop)? 1'b1: 1'b0; 
 	
-	always@(posedge clk)begin
-		if(pre_st == start) begin 
-			temp_sum <= 0;
-			pre_st <= s0;
-			nxt_st <= s0; 
+	always@(posedge clk or (reset == 1))begin
+		if(reset) begin 
+			pre_st <= start; 
 		end
 		else begin
-			if(pre_st == stop)
-				final_sum = temp_sum[BIT_WIDTH-1:0]; 
-			pre_st<=nxt_st;	
-		end 
+			if(pre_st == start) begin 
+				temp_sum <= 0;
+				pre_st <= s0;
+				nxt_st <= s0; 
+			end
+			else begin
+				if(nxt_st == stop)
+					final_sum = temp_sum[BIT_WIDTH-1:0]; 
+				pre_st<=nxt_st;	
+			end
+		end
 	end 
 	
 	always @(*) begin 
