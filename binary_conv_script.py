b960cc7f379ef7ac0ac334e05caa2c0b08b2cd86
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-11-25 14:58:48 -0800

    MAC and the python script updated, mac updated for array reading from file for inputs

---
+++ binary_conv_script.py
@@ -0,0 +1,68 @@
+## Inputs 
+inp_file = open("matlab_file_inputs.csv", "r"); 
+fetched_str = inp_file.read(); 
+inp_str = ""; 
+inp_str = inp_str + '00000001'
+for i in range(0, len(fetched_str)):
+    if(fetched_str[i] != ','):
+        inp_str = inp_str + fetched_str[i]; 
+fh = open("result_inputs.txt","w")
+
+out_str = ''; 
+for i in range(0,400):
+    for j in range(0, 8):
+        out_str = out_str + inp_str[(i*8)+j];
+    out_str = out_str + '\n';
+
+fh.write(out_str);  
+fh.close(); 
+
+## Theta 1
+inp_file1 = open("matlab_file_theta1.csv", "r"); 
+fetched_str1 = inp_file1.read(); 
+inp_str = ""; 
+out_str = "";
+for i in range(0, len(fetched_str1)):
+    if(fetched_str1[i] != ','):
+        inp_str = inp_str + fetched_str1[i]; 
+fh = open("result_theta1.txt","w")
+
+out_str = ''; 
+for z in range(0, 25):
+    out_str = out_str + '[';
+    for i in range(0,401):
+        for j in range(0, 8):
+            out_str = out_str + inp_str[(z*401*8) +(i)+j*401];
+        if(i != 400):    
+            out_str = out_str + ', ';
+    out_str = out_str + ']'; 
+    if(z != 24):
+        out_str = out_str + ',' + '\n'; 
+
+fh.write(out_str);  
+fh.close(); 
+
+## Theta 2
+inp_file2 = open("matlab_file_theta2.csv", "r"); 
+fetched_str2 = inp_file2.read(); 
+inp_str = ""; 
+out_str = ""; 
+for i in range(0, len(fetched_str2)):
+    if(fetched_str2[i] != ','):
+        inp_str = inp_str + fetched_str2[i]; 
+fh = open("result_theta2.txt","w")
+
+out_str = ''; 
+for z in range(0, 10):
+    out_str = out_str + '[';
+    for i in range(0,26):
+        for j in range(0, 8):
+            out_str = out_str + inp_str[(z*26*8) +(i)+j*26];
+        if(i != 25):    
+            out_str = out_str + ', ';
+    out_str = out_str + ']';
+    if(z != 9):
+        out_str = out_str + ',' + '\n'; 
+
+fh.write(out_str);  
+fh.close(); 
--- mac.v
+++ mac.v
@@ -1,20 +1,28 @@
 
-module mac(output[15:0] result, output done, input clk, input reset, input wire[7:0] inp1, input wire[7:0] inp2, input wire[7:0] inp3); 
+module mac_400(output[15:0] result, output done, input clk, input reset, input wire[8:0] no_of_inputs); 
   
   parameter start = 4'b0000, s0 = 4'b0001, s1 = 4'b0010, s2 = 4'b0011, s3 =4'b0100, s4 = 4'b0101, s5 = 4'b0110, s6 = 4'b0111, s7 = 4'b1000, s8 = 4'b1001, s9 = 4'b1010, s10 = 4'b1011, s11= 4'b1100, s12 = 4'b1101, s14 = 4'b1110, stop = 4'b1111;
-  parameter ITER = 3; 
+ 
   wire[15:0] mul_result, adder_result, adder_acc_input; 
   wire[7:0] mul_input, mult_input1, mult_input2;
   wire mult_done, adder_done, adder_reset, mult_reset; 
-  
-  reg[15:0] final_res, temp_acc_reg = 0;
-  reg[3:0] counter = 0; 
+
+  reg[7:0] inp[400:0]; 
+  reg[15:0] temp_acc_reg = 0;
+  reg[8:0] counter = 0; 
   reg[3:0] next_state = start, current_state = start;      // Be careful with the size of this register.
   reg acc_busy = 0;
   reg[7:0] mult_inp_reg; 
+  integer i =0;
+  integer file_inp; 
   
-  initial begin 
-	mult_inp_reg = inp1; 
+  initial begin
+	file_inp = $fopen("result_inputs.txt","r");
+	for(i = 0; (!$feof(file_inp)); i= i+1) begin
+		$fscanf(file_inp, "%b\n", inp[i]);
+	end	
+	$fclose(file_inp);
+	mult_inp_reg = inp[0]; 
   end 
   
   assign mult_input1 = mult_inp_reg;
@@ -22,8 +30,8 @@ module mac(output[15:0] result, output done, input clk, input reset, input wire[
   assign adder_acc_input = temp_acc_reg;
   assign mult_reset = (acc_busy == 0)?1'b0:1'b1; 
   assign adder_reset = (mult_done == 1)?1'b0:1'b1; 
-  assign done = (counter==ITER)? 1'b1:1'b0; 
-  assign result = (counter==ITER)?temp_acc_reg:16'bzzzzzzzzzzzzzzz; 
+  assign done = (counter==no_of_inputs)? 1'b1:1'b0; 
+  assign result = (counter==no_of_inputs)?temp_acc_reg:16'bzzzzzzzzzzzzzzz; 
     
   mult16 m1(mul_result, mult_done, mult_reset, clk, mult_input1, mult_input2); 
   adder16 a1(.result(adder_result), .done(adder_done), .clk(clk), .reset(adder_reset), .x(mul_result), .y(adder_acc_input)); 
@@ -32,16 +40,13 @@ module mac(output[15:0] result, output done, input clk, input reset, input wire[
 	temp_acc_reg = adder_result;
 	acc_busy = 1'b1;
 
-	if(counter < ITER) begin 
+	if(counter < no_of_inputs) begin 
 		counter = counter + 1;
-		 if(counter != ITER)
+		 if(counter != no_of_inputs)
 			#5 acc_busy = 1'b0;
 	end
-	if(counter == 1) 
-	  mult_inp_reg = inp2; 
-	else if(counter == 2) 
-	  mult_inp_reg = inp3; 
-	
+	if(counter != no_of_inputs) 
+	  mult_inp_reg = inp[counter]; 
   end 
 
 endmodule 
